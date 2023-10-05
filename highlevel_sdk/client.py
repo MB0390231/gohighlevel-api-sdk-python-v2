25eae04926ebeadd4aeaaf87e3902bc663a78bf4
Michael <93745624+MB0390231@users.noreply.github.com>
2023-10-04 21:37:15 -0500

    client, response, request, cursor

---
+++ highlevel_sdk/client.py
@@ -0,0 +1,234 @@
+from requests import request
+from copy import deepcopy
+
+from highlevel_sdk.config import HighLevelConfig
+from highlevel_sdk.exceptions import HighLevelError
+
+
+class HighLevelClient(object):
+    """
+    Encapsulates session attributes and methods to make API calls.
+    """
+
+    def __init__(self, access_token) -> None:
+        self.access_token = access_token
+        self.headers = {
+            "Authorization": f"Bearer {self.access_token}",
+            "Content-Type": "application/json",
+        }
+
+    def call(self, method, path, data=None):
+        if method in ("GET", "DELETE"):
+            response = request(method, f"{HighLevelConfig.API_BASE_URL}/{path}", headers=self.headers, params=data)
+        else:
+            response = request(method, f"{HighLevelConfig.API_BASE_URL}/{path}", headers=self.headers, data=data)
+
+        highlevel_response = HighLevelResponse(
+            body=response.text,
+            headers=response.headers,
+            status_code=response.status_code,
+            call={"method": method, "path": path, "params": data, "headers": self.headers},
+        )
+
+        if highlevel_response.is_error():
+            raise highlevel_response.error()
+
+        return highlevel_response
+
+
+class HighLevelResponse(object):
+    """
+    Encapsulates response attributes and methods.
+    """
+
+    def __init__(self, body, headers, status_code, call) -> None:
+        self.body = body
+        self.headers = headers
+        self.status_code = status_code
+        self.call = call
+
+    def is_error(self):
+        return self.status_code >= 400
+
+    def error(self):
+        if self.is_error():
+            return HighLevelError(
+                "Call to HighLevel API was unsuccessful.",
+                body=self.body,
+                headers=self.headers,
+                status_code=self.status_code,
+                call=self.call,
+            )
+        else:
+            return None
+
+    def json(self):
+        return self.body
+
+    def text(self):
+        return self.body
+
+    def __repr__(self):
+        return f"<HighLevelResponse {self.status_code} {self.body}>"
+
+
+class HighLevelRequest(object):
+    """
+    Encapsulates request attributes and methods
+    """
+
+    def __init__(
+        self,
+        method,
+        node,
+        endpoint,
+        api_type=None,
+        param_checker=TypeChecker({}, {}),
+        target_class=None,
+        response_parser=ObjectParser,
+    ) -> None:
+        """
+        Args:
+            method : The HTTP method to use for the request.
+            node : The node to use for the request.
+            endpoint : The endpoint to use for the request.
+            api_type (optional): The type of API call to make.
+            param_checker (optional): The type checker to use for the request.
+            target_class (optional): The class to use for the request.
+            response_parser (optional): The parser to use for the response.
+        """
+        self._method = method
+        self._node = node
+        self._endpoint = endpoint
+        self._api_type = api_type
+        self._path = f"{endpoint}/{node}"
+        self._param_checker = param_checker
+        self._params = {}
+        self._target_class = target_class
+        self._response_parser = response_parser
+
+    def add_param(self, key, value):
+        if not self._param_checker.is_valid_pair(key, value):
+            utils.warning(
+                "value of "
+                + key
+                + " might not be compatible. "
+                + " Expect "
+                + self._param_checker.get_type(key)
+                + "; "
+                + " got "
+                + str(type(value))
+            )
+        else:
+            self._params[key] = self._extract_value(value)
+        return self
+
+    def add_params(self, params):
+        if params is None:
+            return self
+        for key in params.keys():
+            self.add_param(key, params[key])
+        return self
+
+    def _extract_value(self, value):
+        if hasattr(value, "export_all_data"):
+            return value.export_all_data()
+        elif isinstance(value, list):
+            return [self._extract_value(item) for item in value]
+        elif isinstance(value, dict):
+            return dict((self._extract_value(k), self._extract_value(v)) for (k, v) in value.items())
+        else:
+            return value
+
+    def execute(self):
+        params = deepcopy(self._params)
+        if self._api_type == "EDGE" and self._method == "GET":
+            cursor = Cursor(
+                target_objects_class=self._target_class,
+                params=params,
+                node=self._node,
+                endpoint=self._endpoint,
+                object_parser=self._response_parser,
+            )
+            cursor.load_next_page()
+            return cursor
+        response = self._api.call(
+            method=self._method,
+            path=self._path,
+            params=params,
+        )
+        if response.error():
+            raise response.error()
+        if self._response_parser:
+            return self._response_parser.parse_single(response.json())
+        else:
+            return response
+
+
+class Cursor(object):
+    """
+    Iterates over pages of data.
+    """
+
+    def __init__(self, target_objects_class, params, node, endpoint, object_parser) -> None:
+        """
+        Args:
+            target_objects_class : an instance the AbstractObject class. Must have an ID
+            params : The parameters to use for the request.
+            node : The node to use for the request.
+            endpoint : The endpoint to use for the request.
+            object_parser : The parser to use for the response.
+        """
+
+        self._target_objects_class = target_objects_class
+        self._params = params
+        self._node = node
+        self._endpoint = endpoint
+        self._path = f"{endpoint}/{node}"
+        self._object_parser = object_parser
+        self._queue = []
+        self._headers = None
+        self._next_page = None
+        self._has_next_page = False
+
+    def __repr__(self):
+        return str(self._queue)
+
+    def __len__(self):
+        return len(self._queue)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if not self._queue and not self.load_next_page():
+            raise StopIteration()
+
+        return self._queue.pop(0)
+
+    def __getitem__(self, index):
+        return self._queue[index]
+
+    def headers(self):
+        return self._headers
+
+    def load_next_page(self):
+        """
+        populates the queue by querying the api for the next page
+
+        returns True if successful, False otherwise
+        """
+        if not self._has_next_page:
+            return False
+        
+        response = self._api.call(
+            method="GET",
+            path=self._path,
+            params=self._params,
+        )
+        self._headers = response.headers()
+
+        if response.error():
+            raise response.error()
+        
+
